902cf21699969cbc0b61f44e77ebace89f32d581
Yaf98 <manuelyg2097@gmail.com>
2020-06-12 19:22:21 -0500

    RBAC and erwin working

--- escolar/resources/views/layouts/app.blade.php
+++ escolar/resources/views/layouts/app.blade.php
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     
@@ -82,39 +82,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
-            <div class="container">
-                @include('partials.alerts')
-                @yield('content')
-            </div>
-        </main>
-    </div>
-     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    @yield('scripts')
-</body>
-</html>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title')</title>
-</head>
-<body>
-    <header>
-        <div class="navbar navbar-dark bg-dark shadow-sm">
-          <div class="container d-flex justify-content-between">
-            <a href="/alumnos" class="navbar-brand d-flex align-items-center">
-              <strong>Página de inicio</strong>
-            </a>
-          </div>
-        </div>
-      </header>
-    <main role="main">
+       <main role="main">
 
         <section class="jumbotron text-center">
           <div class="container">
@@ -123,9 +91,8 @@
         </section>
       
     </main>
-
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    </div>
+     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     @yield('scripts')
 </body>
 </html>
->>>>>>> 5789b4032a6943d03d959fb49d290d15c8590b62
